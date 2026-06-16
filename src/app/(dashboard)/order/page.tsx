c09e2dc9532e4c6927cfba85e022edcf2d717dbe
rashedulhaquerasel1 <rashedulhaquerasel1@gmail.com>
2026-06-16 22:52:19 +0600

    feat: implement orders page with loading skeleton and update sidebar navigation link

---
+++ src/app/(dashboard)/order/page.tsx
@@ -0,0 +1,21 @@
+import Orders from "@/components/Dashboard/Orders/Orders";
+import React, { Suspense } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+
+
+export default function page() {
+  return (
+    <div>
+      <Suspense fallback={<div>
+
+        <Skeleton className="w-full h-10 mb-4" />
+        <Skeleton className="w-full h-10 mb-4" />
+        <Skeleton className="w-full h-10 mb-4" />
+        <Skeleton className="w-full h-10 mb-4" />
+        <Skeleton className="w-20 h-8" />
+      </div>}>
+        <Orders />
+      </Suspense>
+    </div>
+  );
+}
